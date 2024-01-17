3ec0ab3469ae49d257ee2e87627d4428a454fdb3
Фирстов Дмитрий Игоревич <d.firstov@tinkoff.ru>
2024-01-17 13:46:27 +0300

    Add products methods to the shop project

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -1,42 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/products", (HttpClient httpClient) => httpClient.GetFromJsonAsync<string[]>("http://stock:8080/products"))
+    .WithName("GetProducts")
+    .WithOpenApi();
 
-app.MapGet("/weatherforecast", () =>
+app.MapPost("/products/buy", async (string product, HttpClient httpClient) =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
+        await httpClient.PostAsync($"http://stock:8080/products/reserve?product={product}", null);
+        await httpClient.PostAsync($"http://payments:8080/pay?sum={Random.Shared.Next(100, 10000)}", null);
+        return Results.Ok();
     })
-    .WithName("GetWeatherForecast")
+    .WithName("BuyProduct")
     .WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}

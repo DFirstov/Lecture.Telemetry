c88e8f4b8787c7ade9d7b4c9bcd0520d31177b46
Фирстов Дмитрий Игоревич <d.firstov@tinkoff.ru>
2024-01-17 15:10:52 +0300

    Add product methods to the stock project

--- Stock/Program.cs
+++ Stock/Program.cs
@@ -1,42 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+string[] products = ["milk", "bread", "cheese", "apples", "oranges", "bananas", "eggs", "chicken", "fish", "tomatoes"];
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/products", () => products)
+    .WithName("GetProducts")
+    .WithOpenApi();
+
+app.MapPost("/products/reserve", (string product) =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
+        if (!products.Contains(product))
+            return Results.BadRequest("Product not found");
+        
+        if (Random.Shared.Next(0, 10) == 0)
+            return Results.BadRequest("Product out of stock");
+        
+        return Results.Ok();
     })
-    .WithName("GetWeatherForecast")
+    .WithName("ReserveProduct")
     .WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}

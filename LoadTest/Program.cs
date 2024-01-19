ae1aa9b95336dcefdc27203bf3876914c124edcc
Фирстов Дмитрий Игоревич <d.firstov@tinkoff.ru>
2024-01-19 12:59:46 +0300

    Add a load test project

---
+++ LoadTest/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Http.Json;
+
+HttpClient httpClient = new();
+
+while (true)
+{
+    try
+    {
+        await Task.Delay(Random.Shared.Next(0, 1000));
+
+        var products = await httpClient.GetFromJsonAsync<string[]>("http://shop:8080/products");
+        if (products == null)
+            continue;
+
+        var productIndex = Random.Shared.Next(0, products.Length);
+        var product = products[productIndex];
+
+        await httpClient.PostAsync($"http://shop:8080/products/buy?product={product}", null);
+    }
+    catch
+    {
+        // ignore
+    }
+}
